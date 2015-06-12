6b4ae5e7356f184f108d1c06582e9ccc70a314d3
Elson <ElsonFilho@users.noreply.github.com>
2015-06-12 15:48:02 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming Assignment 2:  Lexical Scoping 
+#
+# Elson Felix Mendes Filho - pricotu@outlook.com
+# 12.06.2015
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = numeric(), nrow, ncol) {
+  i <- NULL
+  set <- function(y, nrow, ncol) {
+    x <<- matrix(y, nrow, ncol)
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+# should retrieve the inverse from the cache.
+# NOTE: For this assignment, assume that the matrix supplied is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
+
+
+#Usage Examples
+
+# Example 1
+M <- matrix(c(1,-0.25, -0.25, 1), nrow = 2, ncol = 2, byrow=TRUE)
+M1 <- makeCacheMatrix(M)
+M1$get()
+cacheSolve(M1)
+
+
+# Example 2
+M1 <- makeCacheMatrix(matrix(c(1,0,4, 1,3,4, 4,1,0), nrow = 3, ncol = 3, byrow=TRUE))
+M1$get()
+cacheSolve(M1)
